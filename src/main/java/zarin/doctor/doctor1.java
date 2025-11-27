a2b552b269012dfac56800aef4d164ba0d44d539
ZarinTasnim27 <zarintasnim160@gmail.com>
2025-11-27 14:36:02 +0600

    2nd commit

--- src/main/java/zarin/doctor/doctor1.java
+++ src/main/java/zarin/doctor/doctor1.java
@@ -1,4 +1,35 @@
 package zarin.doctor;
 
 public class doctor1 {
+    private String name;
+    private int ID;
+
+    public doctor1(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "doctor1{" +
+                "name='" + name + '\'' +
+                ", ID=" + ID +
+                '}';
+    }
 }
